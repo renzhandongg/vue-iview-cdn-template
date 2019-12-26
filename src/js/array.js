3ae4aed851faf4c78c12ed3b25f8a42cb7e97f6c
199038ren <654818685@qq.com>
2019-12-26 21:47:22 +0800

    Array.js添加

---
+++ src/js/array.js
@@ -0,0 +1,72 @@
+//数组扁平化
+// export const flatten = (arr,depth = 1) =>
+//     arr.reduce((item,v) => item.concat((depth>1 && Array.isArray(v)) ? flatten(v,depth - 1):v),[]);
+// //返回数组中某值得所有索引
+// export const indexOffAll = (arr,val) =>
+//     arr.reduce((acc,el,i) => (el === val ? [...acc,i] : acc),[])
+//两数组的交集
+// export const intersection = (a,b) => {
+//     const s = new Set(b);
+//     return a.filter(x => s.has(x));
+// }
+
+//返回指定长度的升序数组
+// export const minN = (arr,n=1) => arr.sort((a,b) => a - b).slice(0,n);
+
+//根据条件反向筛选
+// export const negate = func => (...args) => !func(...args);
+// console.log([1,2,3,4].filter(negate(n => n%2 ===0)))
+
+//生成两数之间指定长度的随机数组
+// export const  randomInteArrrayInRange = (max,min,n=1) =>
+//     Array.from({ length: n }, () => Math.floor(Math.random() * (max - min + 1)) + min);
+
+//在指定数组中获取随机数
+// export const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+//"洗牌数组"
+// export const shuffle = ([...arr]) => {
+//     let m = arr.length;
+//     while(m){
+//         const i = Math.floor(Math.random() * m--);
+//         [arr[m],arr[i]] = [arr[i],arr[m]]
+//     }
+//     return arr;
+// }
+
+
+//nest : 根据parent_id生成树结构
+// export const nest = (items, id = null, link = 'parent_id') =>
+//     items
+//         .filter(item => item[link] === id)
+//         .map(item => ({ ...item, children: nest(items, item.id) }));
+// let items = [
+//     {
+//         id:1,parent_id:null
+//     },
+//     {
+//         id:2,parent_id:1
+//     },
+//     {
+//         id:3,parent_id:1
+//     },
+//     {
+//         id:4,parent_id:2
+//     },
+//     {
+//         id:5,parent_id:3
+//     }
+// ];
+// let nestedComments = nest(items);
+
+
+//计算函数执行时间
+// export const timeTaken = callBack => {
+//     console.time('timeTaken');
+//     const r = callBack();
+//     console.timeEnd('timeTaken');
+//     return r;
+// }
+//
+// let r = timeTaken(() => Math.pow(2,10));
+
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -5,11 +5,15 @@
   </div>
 </template>
 <script>
+import {indexOffAll} from "@/js/array.js"
 import VirtualList from "@/components/virtualList";
 import FlipClock from "@/components/FlipClock.vue";
 let d = [];
 for (let i = 0; i < 1000; i++) {
   d.push({ id: i, value: i });
+}
+function de(arr,deep = 1){
+  return arr.reduce((acc,el) => acc.concat(deep>1 && Array.isArray(el) ? de(el,deep-1) : el),[])
 }
 export default {
   components: {
@@ -20,8 +24,9 @@ export default {
     return {
       data: d
     };
+  },
+  mounted(){
+    console.log(de([1,2,3,[4,[5]]],4))
   }
 };
 </script>
-<style lang="scss">
-</style>
